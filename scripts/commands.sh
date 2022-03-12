9b685b4b177231d04658561cc942bf20520a2c64
Julian-dev28 <julian_martinez28@outlook.com>
2022-03-11 20:48:16 -0800

    Update commands.sh

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,7 +1,5 @@
 npx hardhat run scripts/1-deploy.js --network localhost
-export ADDRESS=
-export TOKENID=
-export ACCOUNT=
+
 npx hardhat run scripts/2-sign.js --network localhost
-export SIGNATURE=
+
 npx hardhat run scripts/3-register.js --network localhost
